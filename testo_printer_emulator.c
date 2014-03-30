866d91cdfd88b6dc3e1a68992f4bd0597458bb83
Kristoffer Ek <stoffer@skulp.net>
2014-03-30 18:02:22 +0200

    regression - no bi-phase code, but no lost frames

--- testo_printer_emulator.c
+++ testo_printer_emulator.c
@@ -7,7 +7,6 @@
 #include "testo_printer_emulator.h"
 
 #define DEBUG
-//#define foo
 
 unsigned char i;
 unsigned long timer_0_ms;
@@ -109,7 +108,6 @@ static void isr_high_prio(void) __interrupt 1 {
 						ir_proto.start_bit_len++;
 					}
 					else {
-						ir_proto.err_corr_bits = 0;			// maybe not necesarry?
 						ir_proto.err_corr_bits_len = 0;
 						ir_proto.state = ERR_CORR_WAIT;
 						_debug();
@@ -123,73 +121,20 @@ static void isr_high_prio(void) __interrupt 1 {
 			case ERR_CORR_WAIT:
 				if (ir_proto.err_corr_bits_len < 4) {
 					if ((TICK_LOW < timer_0) && (timer_0 < TICK_HIGH)) {				// odd
+						_debug();
 						//ir_proto.start_bit = 0b111;
 						//ir_proto.state = INIT_STATE;//ERR_CORR_1;
-#ifdef foo
-						if ((ir_proto.err_corr_bits & (1 << ir_proto.err_corr_bits_len + 1)) != 0) {
-							// previous bit is set
-							ir_proto.err_corr_bits << 1;		// bitshift once to left
-							ir_proto.err_corr_bits &= 0b1110;	// and clear bit 0
-							_debug();
-						}
-						else {
-							// previous bit is zero
-							ir_proto.err_corr_bits << 1;		// bitshift once to left
-							ir_proto.err_corr_bits |= 0b0001;	// and set bit 0
-							_debug();
-							_debug();
-							_debug();
-						}
-#else
-						//_debug();
-						//_debug();
-						//_debug();
-#endif
 					}
 					else if ((2 * TICK_LOW < timer_0) && (timer_0 < 2 * TICK_HIGH)) {	// even
+						_debug();
+						_debug();
+						_debug();
 						//ir_proto.state = INIT_STATE;//ERR_CORR_1;
-#ifdef foo
-						if ((ir_proto.err_corr_bits & (1 << ir_proto.err_corr_bits_len + 1)) != 0) {
-							// previous bit is set
-							ir_proto.err_corr_bits << 1;		// bitshift once to left
-							ir_proto.err_corr_bits |= 0b0001;	// and set bit 0
-							_debug();
-							_debug();
-							_debug();
-						}
-						else {
-							// previous bit is zero
-							ir_proto.err_corr_bits << 1;		// bitshift once to left
-							ir_proto.err_corr_bits &= 0b1110;	// and clear bit 0
-							_debug();
-						}
-#else
-						//_debug();
-#endif
+	//					err_corr_bits_len ? ((err_corr_bits & (1 << err_corr_bits_len - 1)) != 0) : 0		// last bit
 					}
 					else if ((3 * TICK_LOW < timer_0) && (timer_0 < 3 * TICK_HIGH)) {	// odd
-						//_debug();
+						_debug();
 						//ir_proto.state = INIT_STATE;//ERR_CORR_1;
-#ifdef foo
-						if ((ir_proto.err_corr_bits & (1 << ir_proto.err_corr_bits_len + 1)) != 0) {
-							// previous bit is set
-							ir_proto.err_corr_bits << 1;		// bitshift once to left
-							ir_proto.err_corr_bits &= 0b1110;	// and clear bit 0
-							_debug();
-						}
-						else {
-							// previous bit is zero
-							ir_proto.err_corr_bits << 1;		// bitshift once to left
-							ir_proto.err_corr_bits |= 0b0001;	// and set bit 0
-							_debug();
-							_debug();
-							_debug();
-						}
-#else
-						//_debug();
-						//_debug();
-						//_debug();
-#endif
 					}
 					else {
 						//ir_proto.start_bit = 0b000;
@@ -199,12 +144,7 @@ static void isr_high_prio(void) __interrupt 1 {
 				}
 				else {
 					//ir_proto.start_bit = 0b000;
-					/*
 					ir_proto.state = INIT_STATE;//START_BIT_WAIT_1;
-					for (i = 0; i < ir_proto.err_corr_bits; i++) {
-						_debug();
-					}
-					*/
 				}
 				break;
 		}
