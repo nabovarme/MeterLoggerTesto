546619a3499ac4eb34c8bd09ce9b89ddd27f052f
Kristoffer Ek <stoffer@skulp.net>
2014-04-10 23:25:27 +0200

    working error correction

--- testo_printer_emulator.h
+++ testo_printer_emulator.h
@@ -1,3 +1,14 @@
+#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
+#define BYTETOBINARY(byte)  \
+	(byte & 0x80 ? 1 : 0), \
+	(byte & 0x40 ? 1 : 0), \
+	(byte & 0x20 ? 1 : 0), \
+	(byte & 0x10 ? 1 : 0), \
+	(byte & 0x08 ? 1 : 0), \
+	(byte & 0x04 ? 1 : 0), \
+	(byte & 0x02 ? 1 : 0), \
+	(byte & 0x01 ? 1 : 0)
+
 //#define TIMER0_RELOAD 0xFCC8	// 427us @ 8MHz
 //#define TIMER0_RELOAD 0xF000	// ~ 2ms @ 8MHz
 #define TIMER0_RELOAD 0xF323	// ~ 4 * 427us @ 8MHz
@@ -20,5 +31,3 @@ unsigned char reverse(unsigned char b);
 unsigned char valid_err_corr(unsigned int c);
 
 void _debug();
-void _debug2();
-void _debug3();
