885248d2b3cc9bb4ab0876b878d08761bb83d6e1
st0ff3r <stoffer@skulp.net>
2014-05-28 21:56:13 +0200

    kmpGetSerialNo

--- kmp_test.pl
+++ kmp_test.pl
@@ -7,30 +7,14 @@ use Device::SerialPort;
 use Digest::CRC;
 
 
-my $tmp = kmpGetType();
-warn hexdump(data => $tmp, suppress_warnings => 1);
-
 my $port_obj = new Device::SerialPort('/dev/ttyUSB0') || die "$!\n";
 $port_obj->baudrate(1200);
 $port_obj->databits(8);
 $port_obj->stopbits(2);
 $port_obj->parity("none");
 
-# send
-$port_obj->write($tmp);
-
-
-# receive
-my $res = '';
-my ($c, $s);
-do {
-	($c, $s) = $port_obj->read(255);
-	if ($c) {
-		$res .= $s;
-	}
-} while (ord($s) != 13);
-
-warn hexdump(data => $res, suppress_warnings => 1);
+kmpGetType();
+kmpGetSerialNo();
 
 
 
@@ -43,6 +27,7 @@ sub kmpGetType {
 	my $cid = chr(0x01);
 	my $stop_byte = chr(0x0d);
 	
+	# calculate crc
 	my $i;
 	my $data = $dst . $cid;
 	for ($i = 0; $i < length($data); $i++) {
@@ -51,11 +36,60 @@ sub kmpGetType {
 	my $calculated_crc = $ctx->digest;
 	my $calculated_crc_low = chr($calculated_crc & 0xff);
 	my $calculated_crc_high = chr($calculated_crc >> 8);
-	return $start_byte . $dst . $cid . $calculated_crc_high . $calculated_crc_low . $stop_byte;
+
+	# send
+	warn hexdump(data => $start_byte . $dst . $cid . $calculated_crc_high . $calculated_crc_low . $stop_byte, suppress_warnings => 1);
+	$port_obj->write($start_byte . $dst . $cid . $calculated_crc_high . $calculated_crc_low . $stop_byte);
+
+	# receive
+	my $res = '';
+	my ($c, $s);
+	do {
+		($c, $s) = $port_obj->read(255);
+		if ($c) {
+			$res .= $s;
+		}
+	} while (ord($s) != 13);
+
+	warn hexdump(data => $res, suppress_warnings => 1);
+	return $res;
 }
 
 sub kmpGetSerialNo {
+	my $ctx = Digest::CRC->new(width=>16, init=>0x0000, xorout=>0x0000, 
+							refout=>0, poly=>0x1021, refin=>0, cont=>0);
 	
+	my $start_byte = chr(0x80);
+	my $dst = chr(0x3f);
+	my $cid = chr(0x02);
+	my $stop_byte = chr(0x0d);
+	
+	# calculate crc
+	my $i;
+	my $data = $dst . $cid;
+	for ($i = 0; $i < length($data); $i++) {
+		$ctx->add(substr($data, $i, 1));
+	}
+	my $calculated_crc = $ctx->digest;
+	my $calculated_crc_low = chr($calculated_crc & 0xff);
+	my $calculated_crc_high = chr($calculated_crc >> 8);
+
+	# send
+	warn hexdump(data => $start_byte . $dst . $cid . $calculated_crc_high . $calculated_crc_low . $stop_byte, suppress_warnings => 1);
+	$port_obj->write($start_byte . $dst . $cid . $calculated_crc_high . $calculated_crc_low . $stop_byte);
+
+	# receive
+	my $res = '';
+	my ($c, $s);
+	do {
+		($c, $s) = $port_obj->read(255);
+		if ($c) {
+			$res .= $s;
+		}
+	} while (ord($s) != 13);
+
+	warn hexdump(data => $res, suppress_warnings => 1);
+	return $res;
 }
 
 sub kmpSetClock {
