252e2346a0eb79ef51b8a53b7882002b43fe1274
st0ff3r <stoffer@skulp.net>
2014-06-21 22:57:17 +0200

    gpl header added

--- meter_logger.c
+++ meter_logger.c
@@ -1,3 +1,6 @@
+//  Copyright (c) 2014 Johannes Gaardsted Jørgensen <johannesgj@gmail.com> + Kristoffer Ek <stoffer@skulp.net>. All rights reserved.
+//  This program is distributed under the terms of the GNU General Public License
+
 #include <pic18fregs.h>
 #include <stdlib.h>
 #include <stdio.h>
